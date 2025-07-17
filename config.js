c25f8f2f94e29e744ae3d9079f5791c1c0912f67
ArslanMD Official <numberkliye@gmail.com>
2025-07-17 14:59:24 +0500

    ARSLAN-MD

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ARSL~3sZlFJ5Y#-SW6EUi4Z2JD03-DgvTrYXpVCgDCQ5ATaIDuJ2EIofY",
+SESSION_ID: process.env.SESSION_ID || "ARSL~Kg5w0DQR#48jd13S-fqs36r0YMENW4nQdPCBeR6mApuHkNnbX76o",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
