5b3226e27a543c6075a9715424e2be4bf9a45cef
ArslanMD Official <numberkliye@gmail.com>
2025-07-31 13:41:50 +0500

    ARSLAN-MD

--- plugins/menu-new.js
+++ plugins/menu-new.js
@@ -71,7 +71,7 @@ cmd({
             try {
                 await new Promise(resolve => setTimeout(resolve, 1000)); // Small delay after image
                 await conn.sendMessage(from, {
-                    audio: { url: 'https://files.catbox.moe/wzodz1.mp3' },
+                    audio: { url: 'https://files.catbox.moe/m2lh76.mp3' },
                     mimetype: 'audio/mp4',
                     ptt: true,
                 }, { quoted: mek });
